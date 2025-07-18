50ee5b57b3a6a29648964e511d05b7743c0f5140
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-07-18 08:10:52 -0300

    Consumo

--- java/estrutura-sequencial/Consumo.java
+++ java/estrutura-sequencial/Consumo.java
@@ -1,3 +1,24 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Consumo {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int distancia;
+        double combustivel, consumo;
+
+        System.out.print("Distancia percorrida: ");
+        distancia = sc.nextInt();
+        System.out.print("Combustível gasto: ");
+        combustivel = sc.nextDouble();
+
+        consumo = distancia / combustivel;
+
+        System.out.println("Consumo medio = " + String.format("%.3f", consumo));
+
+        sc.close();
+    }
 }
