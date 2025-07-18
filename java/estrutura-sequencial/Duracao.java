3b20fe741898d9c259d24fa80f30a32b41761a05
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-07-18 08:33:35 -0300

    Duracao

--- java/estrutura-sequencial/Duracao.java
+++ java/estrutura-sequencial/Duracao.java
@@ -1,3 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Duracao {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int quant_seg, hora, minuto, segundo, resto;
+
+        System.out.print("Digite a duracao em segundos: ");
+        quant_seg = sc.nextInt();
+
+        hora = quant_seg / 3600;
+        resto = quant_seg % 3600;
+        minuto = resto / 60;
+        segundo = resto % 60;
+
+        System.out.println(hora + ":" + minuto + ":" + segundo);
+
+        sc.close();
+
+    }
 }
