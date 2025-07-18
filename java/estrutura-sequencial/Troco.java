289d4d436e5d4662adeb9c0726a07629d761706e
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-07-17 21:16:58 -0300

    Troco

--- java/estrutura-sequencial/Troco.java
+++ java/estrutura-sequencial/Troco.java
@@ -1,5 +1,26 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Troco {
   public static void main(String[] args) {
-    
+    Locale.setDefault(Locale.US);
+    Scanner sc = new Scanner(System.in);
+
+    double preco, dinheiro, total, troco;
+    int quant;
+
+    System.out.print("Preço unitário do produto: ");
+    preco = sc.nextDouble();
+    System.out.print("Quantidade comprada: ");
+    quant = sc.nextInt();
+    System.out.print("Dinheiro recebido: ");
+    dinheiro = sc.nextDouble();
+
+    total = preco * quant;
+    troco = dinheiro - total;
+
+    System.out.println("TROCO = " + String.format("%.2f", troco));
+
   }  
 }
