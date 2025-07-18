155097bb234a61193a179cea82d4a77c58e28012
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-07-18 11:00:08 -0300

    Operadora

--- java/estrutura-condicional/Operadora.java
+++ java/estrutura-condicional/Operadora.java
@@ -1,3 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Operadora {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int quant_minutos, excedente;
+        double total;
+
+        System.out.print("Digite a quantidade de minutos: ");
+        quant_minutos = sc.nextInt();
+
+        if (quant_minutos <= 100) {
+            System.out.println("Valor a pagar: R$ 50.00");
+        } else {
+            excedente = quant_minutos - 100;
+            total = 50 + (excedente * 2);
+            System.out.println("Valor a pagar: R$ " + String.format("%.2f", total));
+        }
+
+        sc.close();
+    }
 }
