13a3f0352cc4b2610ab4bf1854b4ad9e325e99f5
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-07-18 10:40:07 -0300

    MenorDeTres

--- java/estrutura-condicional/MenorDeTres.java
+++ java/estrutura-condicional/MenorDeTres.java
@@ -1,3 +1,32 @@
+import java.util.Scanner;
+
 public class MenorDeTres {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a, b, c, menor;
+
+        System.out.print("Primeiro valor: ");
+        a = sc.nextInt();
+        System.out.print("Segundo valor: ");
+        b = sc.nextInt();
+        System.out.print("Terceiro valor: ");
+        c = sc.nextInt();
+
+        menor = 0;
+
+        if (a <= b && a <= c) {
+            menor = a;
+        } else {
+            if (b <= a && b <= c) {
+                menor = b;
+            } else {
+                menor = c;
+            }
+        }
+
+        System.out.println("MENOR = " + menor);
+
+        sc.close();
+    }
 }
