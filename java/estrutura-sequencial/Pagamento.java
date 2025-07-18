3c27fb7b1c2e1e6b0714c484edf308d09b0e807b
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-07-17 21:26:49 -0300

    Pagamento

--- java/estrutura-sequencial/Pagamento.java
+++ java/estrutura-sequencial/Pagamento.java
@@ -1,3 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Pagamento {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        String nome;
+        double valor_hora, salario;
+        int horas;
+
+        System.out.print("Nome: ");
+        nome = sc.nextLine();
+        System.out.print("Valor por hora: ");
+        valor_hora = sc.nextDouble();
+        System.out.print("Horas trabalhadas: ");
+        horas = sc.nextInt();
+
+        salario = valor_hora * horas;
+
+        System.out.println("O pagamento para " + nome + " deve ser " + String.format("%.2f", salario));
+
+        sc.close();
+    }
 }
