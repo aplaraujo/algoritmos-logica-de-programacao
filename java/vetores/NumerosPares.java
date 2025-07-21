26b231f97bf8954c1246e959f454f70e4b2c3fdd
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-07-21 16:15:45 -0300

    NumerosPares

--- java/vetores/NumerosPares.java
+++ java/vetores/NumerosPares.java
@@ -1,3 +1,41 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class NumerosPares {
-    
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int i, N, quant, soma;
+        double media;
+
+        quant = 0;
+        soma = 0;
+
+        System.out.print("Quantos elementos vai ter o vetor? ");
+        N = sc.nextInt();
+
+        int[] num = new int[N]; 
+
+        for (i = 0; i < N; i++) {
+           System.out.print("Digite um numero: ");
+           num[i] = sc.nextInt();
+        }
+
+        for (i = 0; i < N; i++) {
+           if (num[i] % 2 == 0) {
+            soma = soma + num[i];
+            quant++;
+           }
+        }
+
+        if (quant > 0) {
+            media = (double) soma / quant;
+            System.out.printf("MEDIA DOS PARES = %.1f %n", media);
+        } else {
+            System.out.println("NENHUM NUMERO PAR");
+        }
+
+        sc.close();
+    }
 }
