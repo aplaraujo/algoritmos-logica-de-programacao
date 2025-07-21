41e5b78dd67b35eea1688f0d7b38210d4488a874
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-07-21 10:24:51 -0300

    Quadrante

--- java/estruturas-repetitivas/Quadrante.java
+++ java/estruturas-repetitivas/Quadrante.java
@@ -1,3 +1,38 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Quadrante {
-    
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double x, y;
+
+        System.out.println("Digite os valores das coordenadas X e Y: ");
+        x = sc.nextDouble();
+        y = sc.nextDouble();
+
+        do {
+            if (x > 0 && y > 0) {
+                System.out.println("QUADRANTE Q1");
+            }
+
+            if (x < 0 && y > 0) {
+                System.out.println("QUADRANTE Q2");
+            }
+
+            if (x < 0 && y < 0) {
+                System.out.println("QUADRANTE Q3");
+            }
+
+            if (x > 0 && y < 0) {
+                System.out.println("QUADRANTE Q4");
+            }
+
+            System.out.println("Digite os valores das coordenadas X e Y: ");
+            x = sc.nextDouble();
+            y = sc.nextDouble();
+        } while (x != 0 && y != 0);
+    }
 }
