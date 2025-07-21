1efef959a9f829e3da506337afa39bee03264e44
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-07-21 11:33:28 -0300

    SequenciaImpares

--- java/estruturas-repetitivas/SequenciaImpares.java
+++ java/estruturas-repetitivas/SequenciaImpares.java
@@ -1,3 +1,21 @@
+
+import java.util.Scanner;
+
 public class SequenciaImpares {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x, i;
+
+        System.out.print("Digite o valor de X: ");
+        x = sc.nextInt();
+
+        for(i = 1; i <= x; i++) {
+            if (i % 2 == 1) {
+               System.out.printf("%d %n", i); 
+            }
+        }
+
+        sc.close();
+    }
 }
