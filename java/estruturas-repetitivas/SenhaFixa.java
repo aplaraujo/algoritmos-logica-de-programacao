b66050cde667b371621e1b56ea48b8b0948d761c
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-07-21 10:15:59 -0300

    SenhaFixa

--- java/estruturas-repetitivas/SenhaFixa.java
+++ java/estruturas-repetitivas/SenhaFixa.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
+
 public class SenhaFixa {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int senha;
+
+        System.out.print("Digite a senha: ");
+        senha = sc.nextInt();
+
+        do {
+            if (senha != 2002) {
+                System.out.print("Senha Invalida! Tente novamente: ");
+                senha = sc.nextInt();
+            }
+        } while (senha != 2002);
+
+        System.out.println("Acesso permitido!");
+
+        sc.close();
+    }
 }
