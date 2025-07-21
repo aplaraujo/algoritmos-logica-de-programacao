0e8c41fafbcacc9d0ed616ce01b95aeb7b7ef3f4
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-07-21 11:49:44 -0300

    ParImpar

--- java/estruturas-repetitivas/ParImpar.java
+++ java/estruturas-repetitivas/ParImpar.java
@@ -1,3 +1,33 @@
+import java.util.Scanner;
+
 public class ParImpar {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num, x, i;
+
+        System.out.print("Quantos numeros voce vai digitar? ");
+        x = sc.nextInt();
+
+        for (i = 0; i < x; i++) {
+            System.out.print("Digite um numero: ");
+            num = sc.nextInt();
+
+            if (num == 0) {
+                System.out.println("NULO");
+            } else {
+                if (num % 2 == 0 && num < 0) {
+                    System.out.println("PAR NEGATIVO");
+                } else if (num % 2 == 0 && num > 0) {
+                    System.out.println("PAR POSITIVO");
+                } else if (num % 2 == 1 && num < 0) {
+                    System.out.println("IMPAR NEGATIVO");
+                } else {
+                    System.out.println("IMPAR POSITIVO");
+                }
+            }
+        }
+
+        sc.close();
+    }
 }
