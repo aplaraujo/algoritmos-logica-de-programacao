42a8e3970e77c4a81dacfc50f3e782f5a9adccf0
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-07-21 15:38:33 -0300

    Alturas

--- java/vetores/Alturas.java
+++ java/vetores/Alturas.java
@@ -1,3 +1,60 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
+
 public class Alturas {
-    
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int i, N, menos_16;
+        double total_altura, media_altura, percentual_menos_16;
+
+        total_altura = 0;
+        menos_16 = 0;
+
+        System.out.print("Quantas pessoas serao digitadas? ");
+        N = sc.nextInt();
+
+        String[] nomes = new String[N];
+        int[] idades = new int[N];
+        double[] alturas = new double[N];
+
+        for (i = 0; i < N; i++) {
+           System.out.printf("Dados da %da pessoa:", i+1);
+           System.out.println();
+           System.out.print("Nome: ");
+           nomes[i] = sc.next();
+           System.out.print("Idade: ");
+           idades[i] = sc.nextInt(); 
+           System.out.print("Altura: ");
+           alturas[i] = sc.nextDouble();
+           sc.nextLine();
+        }
+
+        for (i = 0; i < N; i++) {
+            total_altura = total_altura + alturas[i];
+        }
+
+        for (i = 0; i < N; i++) {
+            if (idades[i] < 16) {
+                menos_16++;
+            }
+        }
+
+        media_altura = total_altura / N;
+        percentual_menos_16 = (double) menos_16 / N * 100;
+
+        System.out.printf("Altura média: %.2f %n%", media_altura);
+        System.out.printf("Pessoas com menos de 16 anos: %.1f%n", percentual_menos_16);
+
+        for (i = 0; i < N; i++) {
+            if (idades[i] < 16) {
+                System.out.println(nomes[i]);
+            }
+        }
+
+        sc.close();
+    }
 }
