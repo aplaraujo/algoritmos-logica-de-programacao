b63aad64e18190895fc4c48f15476d6f264cdd45
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-07-21 13:41:36 -0300

    Fatorial

--- java/estruturas-repetitivas/Fatorial.java
+++ java/estruturas-repetitivas/Fatorial.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
+
 public class Fatorial {
-    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+       int i, N, fat;
+
+       System.out.print("Digite o valor de N: ");
+       N = sc.nextInt();
+
+       fat = 1;
+
+       for (i = 1; i <= N; i++) {
+           fat = fat * i;
+       }
+
+       System.out.printf("FATORIAL = %d", fat);
+
+        sc.close();
+    }
 }
