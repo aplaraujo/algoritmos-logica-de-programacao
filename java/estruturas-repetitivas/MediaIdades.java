b759e24ae617d0c498b79353e821348ac68b4a50
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-07-21 10:09:45 -0300

    Media

--- java/estruturas-repetitivas/MediaIdades.java
+++ java/estruturas-repetitivas/MediaIdades.java
@@ -1,3 +1,36 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class MediaIdades {
-    
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        int idade, soma, i;
+        double media;
+
+        soma = 0;
+        i = 0;
+
+        System.out.println("Digite as idades: ");
+        idade = sc.nextInt();
+
+        if (idade < 0) {
+            System.out.println("IMPOSSIVEL CALCULAR");
+        } else {
+            while (idade > 0) {
+                soma += idade;
+                idade = sc.nextInt();
+                i++;
+            }
+             media = (double) soma / i;
+
+            System.out.printf("MEDIA = %.2f", media);
+        }
+
+       
+
+        sc.close();
+    }
 }
