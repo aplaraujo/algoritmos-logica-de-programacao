f5f8742bee85a4ef25c71685d8a34f8219d4ab1f
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-07-21 11:29:15 -0300

    Tabuada

--- java/estruturas-repetitivas/Tabuada.java
+++ java/estruturas-repetitivas/Tabuada.java
@@ -1,3 +1,20 @@
+
+import java.util.Scanner;
+
 public class Tabuada {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int i, N, mult;
+
+        System.out.print("Deseja a tabuada para qual valor? ");
+        N = sc.nextInt();
+
+        for (i = 1; i <= 10; i++) {
+            mult = N * i;
+            System.out.printf("%d x %d = %d %n", N, i, mult);
+        }
+
+        sc.close();
+    }
 }
