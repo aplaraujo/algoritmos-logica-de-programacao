81ed170248d4db2330d4a34116b37c10d55a7c98
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-07-21 20:53:19 -0300

    Soma

--- java/funcoes/Soma.java
+++ java/funcoes/Soma.java
@@ -1,3 +1,24 @@
+import java.util.Scanner;
+
 public class Soma {
-    
+
+    public static int Obter_Soma(int a, int b) {
+        return a + b;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x, y, soma;
+
+        System.out.print("Digite o valor de X: ");
+        x = sc.nextInt();
+        System.out.print("Digite o valor de Y: ");
+        y = sc.nextInt();
+
+        soma = Obter_Soma(x, y);
+
+        System.out.printf("SOMA = %d", soma);
+
+        sc.close();
+    }
 }
