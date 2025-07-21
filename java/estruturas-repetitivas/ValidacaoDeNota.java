29ae8d45a145b205e3cc294a32ccd715f2d59d7e
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-07-21 10:38:37 -0300

    ValidacaoDeNota

--- java/estruturas-repetitivas/ValidacaoDeNota.java
+++ java/estruturas-repetitivas/ValidacaoDeNota.java
@@ -1,3 +1,34 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ValidacaoDeNota {
-    
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double n1, n2, media;
+
+        System.out.print("Digite a primeira nota: ");
+        do {
+            n1 = sc.nextDouble();
+            if (n1 < 0 || n1 > 10) {
+                System.out.print("Valor invalido! Tente novamente: ");
+            }
+        } while (n1 < 0 || n1 > 10);
+
+        System.out.print("Digite a segunda nota: ");
+        do {
+            n2 = sc.nextDouble();
+            if (n2 < 0 || n2 > 10) {
+                System.out.print("Valor invalido! Tente novamente: ");
+            }
+        } while (n2 < 0 || n2 > 10);
+
+        media = (n1 + n2) / 2;
+
+        System.out.printf("MEDIA = %.2f", media);
+
+        sc.close();
+    }
 }
