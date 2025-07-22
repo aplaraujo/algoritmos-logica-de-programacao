07368712377d2e11a4815e1cc9a1edd1df2042e2
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-07-21 21:08:38 -0300

    Consumo

--- java/funcoes/Consumo.java
+++ java/funcoes/Consumo.java
@@ -1,3 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Consumo {
+
+    public static double Obter_Consumo(double d, double c) {
+        return ((double) d / c);
+    }
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int distancia;
+        double combustivel, consumo;
+
+        System.out.print("Distancia percorrida: ");
+        distancia = sc.nextInt();
+        System.out.print("Combustível gasto: ");
+        combustivel = sc.nextDouble();
+
+        consumo = Obter_Consumo(distancia, combustivel);
+
+        System.out.printf("Consumo medio = %.3f", consumo);
+
+        sc.close();
+    }
     
 }
