08ca84de35eb53df0a3191754d33dc07846f1de2
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-07-22 20:23:27 -0300

    Grade

--- java/estrutura-sequencial/GetFinalGrade.java
+++ java/estrutura-sequencial/GetFinalGrade.java
@@ -1,3 +1,28 @@
+import entities.Grade;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class GetFinalGrade {
-    
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Grade grade = new Grade();
+
+        grade.Name = sc.nextLine();
+        grade.Grade1 = sc.nextDouble();
+        grade.Grade2 = sc.nextDouble();
+        grade.Grade3 = sc.nextDouble();
+
+        System.out.println(grade);
+
+        if (grade.FinalGrade() < 60) {
+            System.out.println("FAILED");
+            System.out.printf("MISSING %.2f POINTS", 60 - grade.FinalGrade());
+        } else {
+            System.out.println("PASS");
+        }
+
+        sc.close();
+    }
 }
--- java/estrutura-sequencial/entities/Grade.java
+++ java/estrutura-sequencial/entities/Grade.java
@@ -1,5 +1,17 @@
 package entities;
 
 public class Grade {
-    
+    public String Name;
+    public double Grade1;
+    public double Grade2;
+    public double Grade3;
+
+    public double FinalGrade() {
+        return Grade1 + Grade2 + Grade3;
+    }
+
+    @Override
+    public String toString() {
+        return "FINAL GRADE = " + String.format("%.2f", FinalGrade());
+    }
 }
