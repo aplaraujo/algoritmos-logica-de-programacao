b2511edb06bbbe7a393e0b618eca11318d8ad63b
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-07-22 18:52:49 -0300

    Idades

--- java/estrutura-sequencial/Idades.java
+++ java/estrutura-sequencial/Idades.java
@@ -1,4 +1,5 @@
 
+import entities.ObterIdades;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -7,26 +8,23 @@ public class Idades {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        String nome1, nome2;
-        int idade1, idade2;
-        double media;
+        ObterIdades idade;
+        idade = new ObterIdades();
 
         System.out.println("Dados da primeira pessoa: ");
         System.out.print("Nome: ");
-        nome1 = sc.nextLine();
+        idade.nome1 = sc.nextLine();
         System.out.print("Idade: ");
-        idade1 = sc.nextInt();
+        idade.idade1 = sc.nextInt();
         sc.nextLine();
 
         System.out.println("Dados da segunda pessoa: ");
         System.out.print("Nome: ");
-        nome2 = sc.nextLine();
+        idade.nome2 = sc.nextLine();
         System.out.print("Idade: ");
-        idade2 = sc.nextInt();
+        idade.idade2 = sc.nextInt();
 
-        media = (double) (idade1 + idade2) / 2;
-
-        System.out.println("A idade média de " + nome1 + " e " + nome2 + " é de " + String.format("%.1f", media) + " anos");
+        System.out.println(idade);
 
         sc.close();
     }
--- java/estrutura-sequencial/entities/ObterIdades.java
+++ java/estrutura-sequencial/entities/ObterIdades.java
@@ -1,5 +1,17 @@
 package entities;
 
 public class ObterIdades {
-    
+    public String nome1;
+    public String nome2;
+    public int idade1;
+    public int idade2;
+
+    public double media() {
+        return ((double) idade1 + idade2) / 2;
+    }
+
+    @Override
+    public String toString() {
+        return "A idade média de " + nome1 + " e " + nome2 + " é de " + media() + " anos";
+    }
 }
