2cc2ba62ac88bd876823bb3da74ad6f84ff6a08a
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-07-22 20:04:47 -0300

    GetNetSalary

--- java/estrutura-sequencial/GetNetSalary.java
+++ java/estrutura-sequencial/GetNetSalary.java
@@ -1,3 +1,31 @@
+import entities.Salary;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class GetNetSalary {
-    
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Salary salary = new Salary();
+        double percentageToIncrease;
+
+        System.out.print("Name: ");
+        salary.Name = sc.nextLine();
+        System.out.print("Gross salary: ");
+        salary.GrossSalary = sc.nextDouble();
+        System.out.print("Tax: ");
+        salary.Tax = sc.nextDouble();
+
+        System.out.println("Employee: " + salary);
+
+        System.out.print("Which percentage to increase salary? ");
+        percentageToIncrease = sc.nextDouble();
+        salary.IncreaseSalary(percentageToIncrease);
+
+        System.out.println("Updated data: " + salary);
+        
+        sc.close();
+
+    }
 }
--- java/estrutura-sequencial/entities/Salary.java
+++ java/estrutura-sequencial/entities/Salary.java
@@ -1,5 +1,20 @@
 package entities;
 
 public class Salary {
-    
+   public String Name;
+   public double GrossSalary;
+   public double Tax;
+
+   public double NetSalary() {
+    return GrossSalary - Tax;
+   }
+
+   public void IncreaseSalary(double percentage) {
+    this.GrossSalary += this.GrossSalary * (percentage / 100);
+   }
+
+   @Override
+   public String toString() {
+    return Name + " $ " + String.format("%.2f%n", NetSalary());
+   }
 }
