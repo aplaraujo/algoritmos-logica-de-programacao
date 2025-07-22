158ddd37ccb0d3087ddba77862a0fded0722ec84
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-07-21 21:01:07 -0300

    Circulo

--- java/funcoes/Circulo.java
+++ java/funcoes/Circulo.java
@@ -1,3 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Circulo {
-    
+
+    public static double Obter_Area(double r) {
+        return (3.14159 * (r * r));
+    }
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double raio, area;
+
+        System.out.print("Digite o valor do raio do circulo: ");
+        raio = sc.nextDouble();
+
+        area = Obter_Area(raio);
+
+        System.out.println("AREA = " + String.format("%.3f", area));
+
+        sc.close();
+    }
 }
