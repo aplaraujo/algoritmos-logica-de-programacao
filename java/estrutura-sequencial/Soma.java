6477efac83b05a837292995943a3afe945f5234b
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-07-22 18:55:36 -0300

    Soma

--- java/estrutura-sequencial/Soma.java
+++ java/estrutura-sequencial/Soma.java
@@ -1,20 +1,20 @@
 
+import entities.ObterSoma;
 import java.util.Scanner;
 
 public class Soma {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int x, y, soma;
+        ObterSoma soma;
+        soma = new ObterSoma();
 
         System.out.print("Digite o valor de X: ");
-        x = sc.nextInt();
+        soma.x = sc.nextInt();
         System.out.print("Digite o valor de Y: ");
-        y = sc.nextInt();
+        soma.y = sc.nextInt();
 
-        soma = x + y;
-
-        System.out.println("SOMA = " + soma);
+        System.out.println(soma);
 
         sc.close();
     }
--- java/estrutura-sequencial/entities/ObterSoma.java
+++ java/estrutura-sequencial/entities/ObterSoma.java
@@ -1,5 +1,15 @@
 package entities;
 
 public class ObterSoma {
-    
+    public int x;
+    public int y;
+
+    public int soma() {
+        return x + y;
+    }
+
+    @Override
+    public String toString() {
+        return "SOMA = " + String.format("%d", soma());
+    }
 }
