dba8f345fdfb82b46cf6d790a226277eadd35390
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-07-21 21:05:12 -0300

    Pagamento

--- java/funcoes/Pagamento.java
+++ java/funcoes/Pagamento.java
@@ -1,3 +1,30 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Pagamento {
-    
+
+    public static double Obter_Sal(double v, double h) {
+        return ((double) v * h);
+    }
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        String nome;
+        double valor_hora, salario;
+        int horas;
+
+        System.out.print("Nome: ");
+        nome = sc.nextLine();
+        System.out.print("Valor por hora: ");
+        valor_hora = sc.nextDouble();
+        System.out.print("Horas trabalhadas: ");
+        horas = sc.nextInt();
+
+        salario = Obter_Sal(valor_hora, horas);
+
+        System.out.printf("O pagamento para %s deve ser %.2f", nome, salario);
+
+        sc.close();
+    }
 }
