b7fbe9df6f52e2b33e64ab05010eb4ef444bddc4
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-07-23 11:38:40 -0300

    Extra

--- java/extra/CelsiusC.java
+++ java/extra/CelsiusC.java
@@ -1,5 +1,7 @@
 package extra;
 
+import extra.util.B2;
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -7,6 +9,15 @@ public class CelsiusC {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+
+        double f, c;
+
+        System.out.print("Digite uma temperatura em Fahrenheit: ");
+        f = sc.nextDouble();
+
+        c = B2.tempC(f);
+
+        System.out.printf("Temperatura equivalente em Celsius: %.0f", c);
 
         sc.close();
     }
--- java/extra/util/B2.java
+++ java/extra/util/B2.java
@@ -1,5 +1,9 @@
 package extra.util;
 
 public class B2 {
-    
+    public static double tempF;
+
+    public static double tempC(double f) {
+        return ((f - 32) * 5) / 9;
+    }
 }
