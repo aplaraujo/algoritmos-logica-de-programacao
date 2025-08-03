a2c787d4eacf5d403475527527ffbdcaa5a6d778
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-08-03 17:17:38 -0300

    Configuração TypeScript

--- java/extra/TypeUtils.java
+++ java/extra/TypeUtils.java
@@ -1,4 +1,10 @@
 package extra;
 
 public class TypeUtils {
+    public static String typeof(Object obj) {
+        if (obj == null) {
+            return "null";
+        }
+        return obj.getClass().getSimpleName();
+    }
 }
