9556763a0e4bd2df5a72d4f8993933097dd91ba5
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-08-12 21:05:48 -0300

    Programacao funcional

---
+++ java/programacao-funcional-lambda/predicado/src/util/ProductPredicate.java
@@ -0,0 +1,12 @@
+package util;
+
+import java.util.function.Predicate;
+
+import entities.Product;
+
+public class ProductPredicate implements Predicate<Product> {
+	@Override
+	public boolean test(Product prod) {
+		return prod.getPrice() >= 100.0;
+	}
+}
