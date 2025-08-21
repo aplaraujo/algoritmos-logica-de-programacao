075c7938438c3bb3473c20680da97a49bee7473e
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-08-21 19:47:54 -0300

    Diagonal negativos

--- c++/diagonal-negativos.cpp
+++ c++/diagonal-negativos.cpp
@@ -6,5 +6,58 @@
 
 using namespace std;
 int main() {
+
+    int i, j, N, quant;
+
+    quant = 0;
+
+    cout << "Qual a ordem da matriz? ";
+    cin >> N;
+
+    int** mat = new int*[N];
+    for (i = 0; i < N; i++)
+        mat[i] = new int[N];
+
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < N; j++)
+        {
+            cout << "Elemento [" << i << "," << j << "]: ";
+            cin >> mat[i][j];
+        }
+        
+    }
+
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < N; j++)
+        {
+            if (mat[i][j] < 0)
+            {
+                quant++;
+            }
+            
+        }
+        
+    }
+    
+    cout << "DIAGONAL PRINCIPAL:" << endl;
+
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < N; j++)
+        {
+            if (i == j)
+            {
+                cout << mat[i][j] << " ";
+            }
+            
+        }
+    }
+
+    cout << "\n";
+
+    cout << "QUANTIDADE DE NEGATIVOS = " << quant << endl;
+
     return 0;
 }
