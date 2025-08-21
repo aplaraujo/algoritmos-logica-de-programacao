d7e6d72fd5ec40e62dbc0ea9e9b23c65cdc9fcf4
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-08-21 18:42:05 -0300

    Crescente

--- c++/crescente.cpp
+++ c++/crescente.cpp
@@ -6,5 +6,29 @@
 
 using namespace std;
 int main() {
+    int x, y;
+
+    cout << "Digite dois numeros: " << endl;
+    cin >> x;
+    cin >> y;
+    do
+    {
+        if (x < y)
+        {
+            cout << "CRESCENTE!" << endl; 
+        }
+        else
+        {
+            cout << "DECRESCENTE!" << endl;
+        }
+
+        cout << "Digite outros dois numeros: " << endl;
+        cin >> x;
+        cin >> y;
+        
+
+    } while (x != y && y != x);
+    
+
     return 0;
 }
