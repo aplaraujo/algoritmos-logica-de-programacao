07f8e85ef4501a78873e6365b39a56942b6fb1e9
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-08-21 17:41:42 -0300

    Conceitos da linguagem C#

--- c++/test.cpp
+++ c++/test.cpp
@@ -1,7 +1,46 @@
 #include <iostream>
-using namespace std;
+#include <iomanip>
+#include <string>
+#include <climits>
 
-int main() {
-    cout << "Hello!!!!!" << endl;
+using namespace std;
+int main()
+{
+    int M, N, i, j;
+
+    cout << "Quantas linhas vai ter a matriz? ";
+    cin >> M;
+    cout << "Quantas colunas vai ter a matriz? ";
+    cin >> N;
+
+    // Dynamically allocate the matrix
+    int** mat = new int*[M];
+    for (i = 0; i < M; i++)
+        mat[i] = new int[N];
+
+    for (i = 0; i < M; i++)
+    {
+        for (j = 0; j < N; j++)
+        {
+            cout << "Elemento [" << i << "," << j << "]: ";
+            cin >> mat[i][j];
+        }
+    }
+    cout << endl
+         << "MATRIZ DIGITADA:" << endl;
+    for (i = 0; i < M; i++)
+    {
+        for (j = 0; j < N; j++)
+        {
+            cout << mat[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    // Free the allocated memory
+    for (i = 0; i < M; i++)
+        delete[] mat[i];
+    delete[] mat;
+
     return 0;
 }
