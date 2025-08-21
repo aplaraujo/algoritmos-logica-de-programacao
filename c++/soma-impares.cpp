a270bb1cbd69b86b52324467ea0153a41d52e454
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-08-21 18:49:44 -0300

    Soma impares

--- c++/soma-impares.cpp
+++ c++/soma-impares.cpp
@@ -6,5 +6,31 @@
 
 using namespace std;
 int main() {
+    int x, y, aux, soma, i;
+
+    soma = 0;
+
+    cout << "Digite dois numeros: " << endl;
+    cin >> x;
+    cin >> y;
+
+    if (x > y)
+    {
+        aux = x;
+        x = y;
+        y = aux;
+    }
+
+    for (i = x+1; i < y; i++)
+    {
+        if (i % 2 != 0)
+        {
+            soma = soma + i;
+        }
+        
+    }
+    
+    cout << "SOMA DOS IMPARES = " << soma << endl;
+
     return 0;
 }
