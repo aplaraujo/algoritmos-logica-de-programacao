550da0bcb3ba97eb7e95cdb17b436344f79a5303
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-08-22 09:31:27 -0300

    Idades

---
+++ c#/Idades/Idades/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Globalization;
+
+CultureInfo ci = CultureInfo.InvariantCulture;
+
+Console.WriteLine("Dados da primeira pessoa:");
+
+Console.Write("Nome: ");
+string nome1 = Console.ReadLine();
+Console.Write("Idade: ");
+int idade1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Dados da segunda pessoa:");
+
+Console.Write("Nome: ");
+string nome2 = Console.ReadLine();
+Console.Write("Idade: ");
+int idade2 = int.Parse(Console.ReadLine());
+
+double media = (double) (idade1 + idade2) / 2;
+
+Console.WriteLine("A idade média de " + nome1 + " e " + nome2 + " é de " + media.ToString("F1", ci) + " anos");
