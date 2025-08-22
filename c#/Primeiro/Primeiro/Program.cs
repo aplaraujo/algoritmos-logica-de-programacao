f0d46a37d42e04269a6723c55bf357a1eee4ffa1
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-08-22 09:11:24 -0300

    Primeiro programa em C#

---
+++ c#/Primeiro/Primeiro/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Globalization;
+
+CultureInfo CI = CultureInfo.InvariantCulture;
+
+double salario;
+
+Console.WriteLine("Digite um salario: ");
+salario = double.Parse(Console.ReadLine(), CI);
+
+Console.WriteLine(salario.ToString("F2", CI));
