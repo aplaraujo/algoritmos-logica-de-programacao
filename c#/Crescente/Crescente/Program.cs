c9e66e975df75ddee5f1958bb4cc7b431087762b
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-08-22 09:41:25 -0300

    Crescente

---
+++ c#/Crescente/Crescente/Program.cs
@@ -0,0 +1,22 @@
+﻿int x, y;
+
+Console.WriteLine("Digite dois numeros:");
+x = int.Parse(Console.ReadLine());  
+y = int.Parse(Console.ReadLine());
+
+do
+{
+    if (x < y)
+    {
+        Console.WriteLine("CRESCENTE!");
+    }
+    else
+    {
+        Console.WriteLine("DECRESCENTE!");
+    }
+
+    Console.WriteLine("Digite outros dois numeros:");
+    x = int.Parse(Console.ReadLine());
+    y = int.Parse(Console.ReadLine());
+}
+while (x != y && y != x);
