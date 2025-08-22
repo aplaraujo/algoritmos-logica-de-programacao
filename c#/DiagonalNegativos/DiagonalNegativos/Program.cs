4738a5c9084775b9a7fb8ac1f1022e03db4878b3
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-08-22 10:09:44 -0300

    Diagonal negativos

---
+++ c#/DiagonalNegativos/DiagonalNegativos/Program.cs
@@ -0,0 +1,45 @@
+﻿int N, i, j, quant;
+
+quant = 0;
+
+
+Console.Write("Qual a ordem da matriz? ");
+N = int.Parse(Console.ReadLine()); 
+
+int[,] mat = new int[N, N];
+
+for(i = 0; i < N; i++)
+{
+    for(j = 0; j < N; j++)
+    {
+        Console.Write("Elemento [" + i + "," + j + "]: ");
+        mat[i, j] = int.Parse(Console.ReadLine());
+    }
+}
+
+for (i = 0; i < N; i++)
+{
+    for (j = 0; j < N; j++)
+    {
+        if (mat[i, j] < 0)
+        {
+            quant++;
+        }
+    }
+}
+
+Console.WriteLine("DIAGONAL PRINCIPAL:");
+
+for (i = 0; i < N; i++)
+{
+    for (j = 0; j < N; j++)
+    {
+        if (i == j)
+        {
+            Console.Write(mat[i, j] + " ");
+        }
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine("QUANTIDADE DE NEGATIVOS = " + quant);
