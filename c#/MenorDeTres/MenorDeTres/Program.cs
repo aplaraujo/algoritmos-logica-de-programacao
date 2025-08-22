73a632b4c1e93f128d85dca617d660db03c1f462
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-08-22 09:36:36 -0300

    Menor de três

---
+++ c#/MenorDeTres/MenorDeTres/Program.cs
@@ -0,0 +1,25 @@
+﻿int a, b, c, menor;
+
+menor = 0;
+
+Console.Write("Primeiro valor: ");
+a = int.Parse(Console.ReadLine());
+Console.Write("Segundo valor: ");
+b = int.Parse(Console.ReadLine());
+Console.Write("Terceiro valor: ");
+c = int.Parse(Console.ReadLine());
+
+if (a < b && a < c)
+{
+    menor = a;
+}
+else if (b < a && b < c)
+{
+    menor = b;  
+}
+else
+{
+    menor = c;
+}
+
+Console.WriteLine("MENOR = " + menor);
