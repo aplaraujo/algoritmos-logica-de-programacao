345966024b1d4409667dc2c4644e9b159b036b77
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-08-22 09:49:57 -0300

    Soma impares

---
+++ c#/SomaImpares/SomaImpares/Program.cs
@@ -0,0 +1,24 @@
+﻿int x, y, i, aux, soma;
+
+soma = 0;
+
+Console.WriteLine("Digite dois numeros:");
+x = int.Parse(Console.ReadLine());
+y = int.Parse(Console.ReadLine());
+
+if (x > y)
+{
+    aux = x;
+    x = y;
+    y = aux;
+}
+
+for (i = x+1; i < y; i++)
+{
+    if (i % 2 != 0)
+    {
+        soma += i;
+    }
+}
+
+Console.WriteLine("SOMA DOS IMPARES = " + soma);
